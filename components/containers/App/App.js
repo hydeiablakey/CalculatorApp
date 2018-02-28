7202bc30cc63662771fbaf64aa521b4eae6b4781
Hydeia <blakeyhydeia@gmail.com>
2018-02-28 17:01:12 -0500

    Added button and input component and created calculator buttons

--- components/containers/App/App.js
+++ components/containers/App/App.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 import Input from '../../presentational/Input/Input';
+import Buttons from '../../presentational/Buttons/Buttons'
+
+import './App.less';
 
 export default class App extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      prevNum: "",
+      currentNum: "",
+      currentOperation: null,
+    }
+  }
 
+  _handleOperators = ( event ) => {
+    const valueOperator = event.target.value;
+    this.setState({ currentOperation: valueOperator });
+  }
 
-	render() {
-		return (
-			<div>
-			<p> .</p>
+  _handleNumbers = ( event ) => {
+    const digit = parseInt( event.target.value );
 
-			<Input /> 
-			</div>
-		);
-	}
+    if( this.state.currentOperation === null ) {
+      //if we aren't adding an operation to currentNumber then we should add digits to the currentnumber. 
+        this.setState({
+        currentNum: this.state.currentNum + digit
+        });
+
+      //If we are adding an operation, then set that currentNum to be prevNum and 
+      if ( this.state.currentOperation ) {
+
+        this.setState({ 
+        prevNum: this.state.currentNum
+        
+        })
+
+        // console.log(this.state);
+      }
+    }
+
+    console.log( this.state );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Input currentNum={ this.state.currentNum } /> 
+        <Buttons handleOperators={ this._handleOperators } handleNumbers={ this._handleNumbers } />
+      </div>
+    );
+  }
 }
--- components/presentational/Input/Input.js
+++ components/presentational/Input/Input.js
@@ -2,25 +2,10 @@ import React, { Component } from 'react';
 import './Input.less';
 
 export default class Input extends Component {
+
   render() {
     return (
-      <div>
-        <div className="container">
-          <input className="inputContainer " value="0" type="text" /> 
-          <div className="buttonContainer">
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <br/>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <br />
-          </div>
-        </div>
-      </div>
+      <div className="calculatorDisplay" type="text" placeholder="0">{ this.props.currentNum }</div>
     );
   }
 }
